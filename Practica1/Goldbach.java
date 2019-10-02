00ad89383e7e39ae8cb7e7eeea124787b938f797
kmagjistari <k.magjistari@alumnos.upm.es>
2019-10-02 10:43:45 +0200

    Arregla la condicion para numero 0 y negativos

--- Practica1/Goldbach.java
+++ Practica1/Goldbach.java
@@ -1,4 +1,3 @@
-
 public class Goldbach {
 	/**
 	 * Checks if the number n is prime. Negative numbers and zero are declared
@@ -8,7 +7,10 @@ public class Goldbach {
 	 *            Number to be tested.
 	 * @return True if n is prime, false if not.
 	 */
-	public static boolean isPrime(Integer n) {
+	public  boolean isPrime(Integer n) {
+		if (n<= 0) {
+			return false;
+		} 
 		for (int p = 2; p < n; p++) {
 			if (n % p == 0) {
 				return false;
@@ -19,18 +21,17 @@ public class Goldbach {
 	}
 
 	/**
-	 * Checks Goldbach conjecture for n, that is, checks if the odd number n
+	 * Checks Goldbach conjecture for n, that is, checks if the even number n
 	 * can be written as the sum of two prime numbers.
 	 * 
-	 * If n is even, it returs automatically true.
+	 * If n is odd, it returs automatically true.
 	 * 
 	 * @param n
 	 *            Number to be checked.
 	 * @return True if n can be decomposed as sum of prime numbers, false if
 	 *         not. Also prints the decomposition if true.
 	 */
-
-	public static boolean checkGoldbach(Integer n) {
+	public boolean checkGoldbach(Integer n) {
 		for (int p = 1; p < n / 2; p++) {
 			if (isPrime(p) && isPrime(n - p)) {
 				System.out.println(n + "=" + p + " & " + (n - p));
